2ffe0ae5c6b2a287673182a6b055f7feba1d7a00
arafasaka <arafasaka@gmail.com>
2020-04-04 21:57:31 +0700

    sql terbaru

--- web_gis.sql
+++ web_gis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2020 at 08:13 PM
+-- Generation Time: Apr 04, 2020 at 04:55 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -45,6 +45,49 @@ INSERT INTO `bangunan` (`bangunan_id`, `bangunan_nama`, `bangunan_lat`, `banguna
 (3, 'Bertiga Cafe', '110.4361753', '-7.0652547'),
 (4, 'Burjo Cafe', '110.4370456', '-7.0673298');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bidang`
+--
+
+CREATE TABLE `bidang` (
+  `bidang_kode` int(11) NOT NULL,
+  `bidang_nama` varchar(200) NOT NULL,
+  `bidang_keterangan` text NOT NULL,
+  `bidang_gambar` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bidang`
+--
+
+INSERT INTO `bidang` (`bidang_kode`, `bidang_nama`, `bidang_keterangan`, `bidang_gambar`) VALUES
+(100, 'Mulawarman V', 'Keterangan Lahan Mulawarman V', 'gambar_1.jpg'),
+(200, 'Es Kelapa Muda', 'Keterangan lahan Es Kelapa muda', 'gambar_2.jpg'),
+(300, 'Kontrakan Gondang Legend', 'Keterangan kontrakan gondang', 'gambar_3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dokumentasi`
+--
+
+CREATE TABLE `dokumentasi` (
+  `dokumentasi_id` int(11) NOT NULL,
+  `dokumentasi_bidang_id` varchar(200) NOT NULL,
+  `dokumentasi_gambar` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dokumentasi`
+--
+
+INSERT INTO `dokumentasi` (`dokumentasi_id`, `dokumentasi_bidang_id`, `dokumentasi_gambar`) VALUES
+(1, '100', 'gambar_10.jpg'),
+(2, '100', 'gambar_11.jpg'),
+(3, '100', 'gambar_12.jpg');
+
 --
 -- Indexes for dumped tables
 --
@@ -54,6 +97,18 @@ INSERT INTO `bangunan` (`bangunan_id`, `bangunan_nama`, `bangunan_lat`, `banguna
 --
 ALTER TABLE `bangunan`
   ADD PRIMARY KEY (`bangunan_id`);
+
+--
+-- Indexes for table `bidang`
+--
+ALTER TABLE `bidang`
+  ADD PRIMARY KEY (`bidang_kode`);
+
+--
+-- Indexes for table `dokumentasi`
+--
+ALTER TABLE `dokumentasi`
+  ADD PRIMARY KEY (`dokumentasi_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
